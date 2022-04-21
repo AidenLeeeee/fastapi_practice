f6c44b7196aa5d1498462f9836adabff158b1a36
AidenLeeeee <hoo8799@naver.com>
2022-04-21 22:17:48 +0900

    #1. FastAPI_tutorial

--- main.py
+++ main.py
@@ -1,13 +1,79 @@
-import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, File, UploadFile, HTTPException, status, Request
+from fastapi.responses import JSONResponse
+from fastapi.staticfiles import StaticFiles
+from tempfile import NamedTemporaryFile
+from typing import IO
+
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+users = {
+    1: {"name": "Fast"},
+    2: {"name": "Campus"},
+    3: {"name": "API"},
+}
 
 
-@app.get("/")
-def hello():
-    return "Hello, World!"
+class SomeError(Exception):
+    def __init__(self, name: str, code: int):
+        self.name = name
+        self.code = code
+
+    def __str__(self):
+        return f"<{self.name}> is occured. code: <{self.code}>"
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+async def save_file(file: IO):
+    # s3 업로드라고 생각해 봅시다. delete=True(기본값)이면
+    # 현재 함수가 닫히고 파일도 지워집니다.
+    with NamedTemporaryFile("wb", delete=False) as tempfile:
+        tempfile.write(file.read())
+        return tempfile.name
+
+
+@app.post("/login")
+def login(username: str = Form(...), password: str = Form(...)):
+    return {"username": username}
+
+
+@app.get("/users/{user_id}")
+async def get_user(user_id: int):
+    if user_id not in users.keys():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"<User: {user_id}> is not exists.",
+        )
+    return users[user_id]
+
+
+@app.post("/file/size")
+def get_filesize(file: bytes = File(...)):
+    return {"file_size": len(file)}
+
+
+@app.post("/file/info")
+def get_file_info(file: UploadFile = File(...)):
+    return {
+        "content_type": file.content_type,
+        "filename": file.filename
+    }
+
+
+@app.post("/file/store")
+async def store_file(file: UploadFile = File(...)):
+    path = await save_file(file.file)
+    return {"filepath": path}
+
+
+# Error
+@app.exception_handler(SomeError)
+async def some_error_handler(request: Request, exc: SomeError):
+    return JSONResponse(
+        content={"message": f"error is {exc.name}"}, status_code=exc.code
+    )
+
+
+@app.get("/error")
+async def get_error():
+    raise SomeError("Hello", 501)
